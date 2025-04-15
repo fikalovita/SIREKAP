f5dedb23cda60dd7c33c8f0fb265eea2e37b80e2
Fika <fikalovita08@gmail.com>
2025-04-15 13:21:46 +0700

    Update sidebar brand logo text to 'SIREKAP' and change login page title to 'SIREKAP | Log in'

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -32,9 +32,9 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="../../index3.html" class="brand-link">
+    <a href="#" class="brand-link">
       <img src="<?= base_url('Assets/') ?>img/rsi.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">RS ISLAM LUMAJANG</span>
+      <span class="brand-text font-weight-light">SIREKAP</span>
     </a>
 
     <!-- Sidebar -->

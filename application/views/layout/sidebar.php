3ffe913b5e93b3bc7c4ce600dcf9b43bb4317de5
Fika <fikalovita08@gmail.com>
2025-05-06 13:23:43 +0700

    feat: Update sidebar navigation to link to RekapSEP page

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -177,7 +177,7 @@
             </ul>
             <ul class="nav nav-treeview" style="display: none;">
               <li class="nav-item">
-                <a href="#" class="nav-link">
+                <a href="<?= base_url('RekapSEP') ?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>SEP</p>
                 </a>
